91000672bf173a5a38f7bbb1f56a3041a57a373e
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-02-05 12:52:16 -0500

    All tests passed

--- PredicateUtilities.java
+++ PredicateUtilities.java
@@ -10,7 +10,14 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+        //if (x>y){
+            return x>y;
+        //system.out.println(y +"is less than"+ x);
+    
+    //else {
+        //return (y>x);
+        //system.out.println(x +"is less than"+ y);
+    
     }
 
     /**
@@ -19,17 +26,38 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
-    }
+        //if (x>y){
+            return x<y;
+        //system.out.println(y +"is less than"+ x);
+    
+    //else {
+        //return (y>x);
+        //system.out.println(x +"is less than"+ y);
+    
+}
 
     /**
      * @param x
      * @param y
      * @return true if `x` is greater than or equal to `y`
      */
-    public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+    public Boolean isGreaterThanOrEqualTo(int x,int y) {
+        
+        //if (x>y){
+            return x>=y;
+            
+       // system.out.println(x +"is Greater than or Equal to"+ y);
+    
+    //else if (x=y){
+        //return(x=y);
+        //system.out.println(y + "+"is Greater than or Equal to"+ x);
+    
+    
+    //else if (y<x) {
+        //return (y<x);
     }
+    
+
 
     /**
      * @param x
@@ -37,21 +65,34 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
-    }
+        
+        //if (x<y){
+            return x<=y;
+            
+       
+    
+    //else if (x=y){
+        //return(x=y);
+      
+    
+    
+    //else if (y<x) {
+        //return (y<x);
+    
+}
     
     
        /**
       * @return true
      */
     public Boolean returnTrue() {
-        return null;
+        return true;
     }
 
     /**
      * @return false
      */
     public Boolean returnFalse() {
-        return null;
+        return false;
     }
 }
